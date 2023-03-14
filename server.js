14f748ef8447dda34db07e1fd88c33998ef967eb
meiqi-gu <77755155+meiqi-gu@users.noreply.github.com>
2023-03-14 19:24:58 -0400

    testing dif loc

--- server.js
+++ server.js
@@ -18,12 +18,21 @@ const port = args.port || 3000;
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
-fs.readFile('./public/index.html', 'utf-8', (err, data) => {
+fs.readFile('./public/index.html', 'utf8', (err, data) => {
     if (err) {
         console.error(err); 
         return; 
     }
-    console.log(data); 
+    const server = http.createServer((req, res) => {
+        res.write(200); 
+        res.setHeader('Header', `text/html`);
+        res.write(data);
+        res.end();  
+    })
+
+    server.listen(port); 
+    console.log(`Server listening on port ${port}`); 
+    
     }); 
 
 // Define a const `server` as an arrow function using http.createServer. 
@@ -32,17 +41,8 @@ fs.readFile('./public/index.html', 'utf-8', (err, data) => {
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
-const server = http.createServer((req, res) => {
-    res.write(200); 
-    res.setHeader('Header', `text/html`);
-    res.write(data);
-    res.end();  
-})
-
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-server.listen(port); 
-console.log(`Server listening on port ${port}`); 
 
 // That's it! You're all done!
