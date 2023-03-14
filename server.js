0fcd46cc37170031bf45f08605ababf9faac5f53
meiqi-gu <77755155+meiqi-gu@users.noreply.github.com>
2023-03-14 17:02:22 -0400

    delete spaces

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = mini(process.argv)
 // Define a const `port` using the argument from the command line. 
 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-if (port === null || port==="") {
+if (port===null || port==="") {
     port = 3000; 
 }
  
