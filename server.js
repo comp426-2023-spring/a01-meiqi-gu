b277093d5ee8672cc49b2162130394ca8d5c3cd1
meiqi-gu <77755155+meiqi-gu@users.noreply.github.com>
2023-03-14 19:16:45 -0400

    add default port

--- server.js
+++ server.js
@@ -5,13 +5,10 @@ const fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
 const mini = require('minimist');
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-const port = mini(process.argv)
+let args = mini(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
-
-// Make this const default to port 3000 if there is no argument given for `--port`.
-if (port===null || port==="") {
-    port = 3000; 
-}
+const port = args.port || 3000; 
+// Make this const default to port 3000 if there is no argument given for `--port`. 
  
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
